f9d9c0f1f136637c7e5167052ba2bd4a70892fde
CarlosQuiroz25 <caquirozm@cesde.net>
2024-09-21 16:47:43 -0500

    Gestion de mascotas

--- src/main/java/com/mascota/gestion/Main.java
+++ src/main/java/com/mascota/gestion/Main.java
@@ -3,6 +3,7 @@ package com.mascota.gestion;
 public class Main {
     public static void main(String[] args) {
         Perro perro = new Perro("Daniel", "Canino", "labrador", 3, "mediano", "Café", "Saludable");
+        perro.hacerSonido();
         perro.alimentar();
         perro.cuidar();
         perro.mostrarInformacion();
@@ -10,6 +11,7 @@ public class Main {
         System.out.println("****************************");
 
         Gato gato = new Gato("Pepe", "Manchado", "Ojos verdes", 5, "Pequeño", "Blanco", "Enfermo");
+        gato.hacerSonido();
         gato.alimentar();
         gato.cuidar();
         gato.mostrarInformacion();
@@ -17,6 +19,7 @@ public class Main {
         System.out.println("****************************");
 
         Pajaro pajaro = new Pajaro("Juan", "Emplumado", "Pico corto", 6, "Pequeño", "Amarillo", "Saludable");
+        pajaro.hacerSonido();
         pajaro.alimentar();
         pajaro.cuidar();
         pajaro.mostrarInformacion();
@@ -24,6 +27,7 @@ public class Main {
         System.out.println("****************************");
 
         Tortuga tortuga = new Tortuga("Lina", "De mar", "Dientona", 20, "Mediano", "Marrón", "Crítico");
+        tortuga.hacerSonido();
         tortuga.alimentar();
         tortuga.cuidar();
         tortuga.mostrarInformacion();
